36c938ffe92af5d7d3f1446e33a7537830e68f91
Ian Taylor <taylorim@gmail.com>
2013-03-11 11:57:03 -0300

    Update categories.rb
    Updated for syntax on api_key and uri.  Added the necessary password of "apikey" for login information.

--- ruby/categories.rb
+++ ruby/categories.rb
@@ -3,14 +3,14 @@ require 'rexml/document'
 
 # Your account_id number
 account_id = 001 
-api_key = 010101010101010101010
+api_key = "010101010101010101010"
 
-uri = URI('https://api.merchantos.com/API/Account/#{account_id}/Category.xml')
+uri = URI("https://api.merchantos.com/API/Account/#{account_id}/Category.xml")
 response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
   request = Net::HTTP::Get.new uri.request_uri
   # or use username and password instead (not recommended)
   # request.basic_auth username, password
-	request.basic_auth api_key
+	request.basic_auth api_key, 'apikey'
 	response = http.request request
 end
 
